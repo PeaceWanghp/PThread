c8adfd145a1b93e661e5e61efa301644050c4581
Peace <peace@bogon>
2018-03-21 10:18:36 +0800

    add project

---
+++ PThread/PThread/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PThread
+//
+//  Created by peace on 2018/3/21.
+//  Copyright © 2018 peace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ PThread/PThread/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  PThread
+//
+//  Created by peace on 2018/3/21.
+//  Copyright © 2018 peace. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
